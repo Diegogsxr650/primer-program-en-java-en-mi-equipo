287ed4551ae38d6ef3ce80f50d7e341035bf672d
Diego <garridod563@gmail.com>
2024-09-23 13:35:44 +0200

    Aquí he añadido mi edad y año de nacimineto

---
+++ Prueba2.java
@@ -0,0 +1,8 @@
+public class Prueba2 {
+       public static void main(String[] args) {
+           System.out.println("Garrido Sánchez");
+	   System.out.println("18");
+           System.out.println("2006");
+          
+       }
+   }
