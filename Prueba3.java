453cc050ffbc91d6639e4302c370e7f7027e05c6
Diego <garridod563@gmail.com>
2024-09-25 13:54:32 +0200

    grupo y artista

--- Prueba3.java
+++ Prueba3.java
@@ -1,8 +1,8 @@
 public class Prueba3 {
        public static void main(String[] args) {
-           System.out.println("Garrido Sánchez");
 	   System.out.println("18");
            System.out.println("2006");
            System.out.println("Guisantes con jamón");
+	   System.out.println("Metallica, James Hetfield"); 	
        }
    }
